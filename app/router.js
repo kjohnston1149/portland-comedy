6dc0d3f7b8ce1f30c829cbdfb3aac7c6301cb46e
Kevin Johnston <kjohnston1149@gmail.com>
2016-12-19 10:12:43 -0800

    Add open-mic route with successful linking

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('open-mic');
 });
 
 export default Router;
---
+++ tests/unit/routes/open-mic-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:open-mic', 'Unit | Route | open mic', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
