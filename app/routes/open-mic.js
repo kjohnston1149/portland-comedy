fd8feb5619536f67cc917c97390d6cdda89179c8
Kevin Johnston <kjohnston1149@gmail.com>
2016-12-20 13:07:19 -0800

    Comment out JS temporarily for display purposes

--- app/routes/open-mic.js
+++ app/routes/open-mic.js
@@ -1,53 +1,81 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-model: function(gapi) {
-  var CLIENT_ID = '474937019011-ck824t8cts8ls81hovh552nn10oovlgd.apps.googleusercontent.com';
-
- var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
-
- function checkAuth() {
-        gapi.auth.authorize(
-          {
-            'client_id': CLIENT_ID,
-            'scope': SCOPES.join(' '),
-            'immediate': true
-          }, handleAuthResult);
-      }
-
-
-
-
-
-     function loadCalendarApi() {
-       var calendarID = 'jim6kbma2850sds9mcohvbmgbo@group.calendar.google.com';
-       checkAuth(CLIENT_ID);
-       handleAuthResult(CLIENT_ID);
-
-        var url = "https://www.googleapis.com/calendar/v3/calendars/" + calendarID;
-        return Ember.$.getJSON(url).then(function(responseJSON) {
-            console.log(responseJSON.results);
-           return responseJSON.results;
-
-      });
-
-     }
-     function handleAuthResult(authResult) {
-
-      var authorizeDiv = document.getElementById('authorize-div');
-      if (authResult && !authResult.error) {
-        // Hide auth UI, then load client library.
-        authorizeDiv.style.display = 'none';
-        loadCalendarApi();
-        console.log(responseJSON.results);
-      } else {
-        // Show auth UI, allowing the user to initiate authorization by
-        // clicking authorize button.
-        authorizeDiv.style.display = 'inline';
-      }
-    }
+  model: function() {
+//     var CLIENT_ID = '474937019011-ck824t8cts8ls81hovh552nn10oovlgd.apps.googleusercontent.com';
+//     var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+//     var calendarID = 'jim6kbma2850sds9mcohvbmgbo';
+//     function checkAuth() {
+//         gapi.auth.authorize(
+//           {
+//             'client_id': CLIENT_ID,
+//             'scope': SCOPES.join(' '),
+//             'immediate': true
+//           });
+//       }
+//
+//      var url = "https://www.googleapis.com/calendar/v3/calendars/" + calendarID;
+//      $.when.function($, url){
+//        return Ember.$.getJSON(url).then(function(responseJSON) {
+//        console.log(responsJSON);
+//        return responseJSON;
+//
+//    });
+//  }
+// }
 
 
-
+// model: function(gapi) {
+//   var CLIENT_ID = '474937019011-ck824t8cts8ls81hovh552nn10oovlgd.apps.googleusercontent.com';
+//
+//  var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+//
+// var calendarID = 'jim6kbma2850sds9mcohvbmgbo@group.calendar.google.com';
+// var url = "https://www.googleapis.com/calendar/v3/calendars/" + calendarID;
+//
+//  function checkAuth() {
+//         gapi.auth.authorize(
+//           {
+//             'client_id': CLIENT_ID,
+//             'scope': SCOPES.join(' '),
+//             'immediate': true
+//           }, handleAuthResult);
+//       }
+//
+//
+//       function handleAuthResult(authResult) {
+//
+//        var authorizeDiv = document.getElementById('authorize-div');
+//        if (authResult && !authResult.error) {
+//          // Hide auth UI, then load client library.
+//          authorizeDiv.style.display = 'none';
+//        } else {
+//          // Show auth UI, allowing the user to initiate authorization by
+//          // clicking authorize button.
+//          authorizeDiv.style.display = 'inline';
+//        }
+//       }
+//
+//
+//      function loadCalendarApi() {
+//
+//        checkAuth().then(handleAuthResult());
+//
+//
+//
+//         return Ember.$.getJSON(url).then(function(responseJSON) {
+//             console.log(responseJSON.results);
+//            return responseJSON.results;
+//
+//       });
+//
+//
+//      }
+//        loadCalendarApi(CLIENT_ID, SCOPES, url);
+//
+//
+//
+//
+// }
 }
 });
