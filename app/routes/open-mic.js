e4736e9452b2042a6a8e31bd4d0d2de4c07d9793
Kevin Johnston <kjohnston1149@gmail.com>
2016-12-21 10:06:43 -0800

    Add working API call in route

--- app/routes/open-mic.js
+++ app/routes/open-mic.js
@@ -1,81 +1,106 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-//   model: function() {
-//     debugger;
-//     var CLIENT_ID = '474937019011-ck824t8cts8ls81hovh552nn10oovlgd.apps.googleusercontent.com';
-//     var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
-//     var calendarID = 'jim6kbma2850sds9mcohvbmgbo';
-//     function checkAuth() {
-//         gapi.auth.authorize(
-//           {
-//             'client_id': CLIENT_ID,
-//             'scope': SCOPES.join(' '),
-//             'immediate': true
-//           });
-//       }
-//
-//      var url = "https://www.googleapis.com/calendar/v3/calendars/" + calendarID;
-//      //checkAuth();
-//        return Ember.$.getJSON(url).then(function(responseJSON) {
-//          console.log(responseJSON);
-//          return responseJSON;
-//        });
-//
-// }
+  model: function () {
+    $.getScript("https://apis.google.com/js/client.js?onload=checkAuth", function() {
+    
+    }).then( function handleAuthClick(event) {
+       gapi.auth.authorize(
+         {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+         handleAuthResult);
+       return false;
+}
 
-// 
-// model: function() {
-//   var CLIENT_ID = '474937019011-ck824t8cts8ls81hovh552nn10oovlgd.apps.googleusercontent.com';
-//
-//  var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
-//
-// var calendarID = 'jim6kbma2850sds9mcohvbmgbo@group.calendar.google.com';
-// var url = "https://www.googleapis.com/calendar/v3/calendars/" + calendarID;
-//
-//  function checkAuth() {
-//         gapi.auth.authorize(
-//           {
-//             'client_id': CLIENT_ID,
-//             'scope': SCOPES.join(' '),
-//             'immediate': true
-//           }, handleAuthResult);
-//       }
-//
-//
-//       function handleAuthResult(authResult) {
-//
-//        var authorizeDiv = document.getElementById('authorize-div');
-//        if (authResult && !authResult.error) {
-//          // Hide auth UI, then load client library.
-//          authorizeDiv.style.display = 'none';
-//        } else {
-//          // Show auth UI, allowing the user to initiate authorization by
-//          // clicking authorize button.
-//          authorizeDiv.style.display = 'inline';
-//        }
-//       }
-//
-//
-//      function loadCalendarApi() {
-//
-//        checkAuth().then(handleAuthResult());
-//
-//
-//
-//         return Ember.$.getJSON(url).then(function(responseJSON) {
-//             console.log(responseJSON.results);
-//            return responseJSON.results;
-//
-//       });
-//
-//
-//      }
-//        loadCalendarApi(CLIENT_ID, SCOPES, url);
-//
-//
-//
-//
-// }
+
+
+    );
+    // Your Client ID can be retrieved from your project in the Google
+    // Developer Console, https://console.developers.google.com
+    var CLIENT_ID = '474937019011-ck824t8cts8ls81hovh552nn10oovlgd.apps.googleusercontent.com';
+
+    var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+    /**
+     * Check if current user has authorized this application.
+     */
+    function checkAuth() {
+      gapi.auth.authorize(
+        {
+          'client_id': CLIENT_ID,
+          'scope': SCOPES.join(' '),
+          'immediate': true
+        }, handleAuthResult);
+    }
+    /**
+     * Handle response from authorization server.
+     *
+     * @param {Object} authResult Authorization result.
+     */
+    function handleAuthResult(authResult) {
+      var authorizeDiv = document.getElementById('authorize-div');
+      if (authResult && !authResult.error) {
+        // Hide auth UI, then load client library.
+        authorizeDiv.style.display = 'none';
+        loadCalendarApi();
+      } else {
+        // Show auth UI, allowing the user to initiate authorization by
+        // clicking authorize button.
+        authorizeDiv.style.display = 'inline';
+      }
+    }
+
+    /**
+     * Initiate auth flow in response to user clicking authorize button.
+     *
+     * @param {Event} event Button click event.
+     */
+
+    function loadCalendarApi() {
+      gapi.client.load('calendar', 'v3', listUpcomingEvents);
+    }
+  function listUpcomingEvents() {
+    var request = gapi.client.calendar.events.list({
+      'calendarId': 'jim6kbma2850sds9mcohvbmgbo@group.calendar.google.com',
+      'timeMin': (new Date()).toISOString(),
+      'showDeleted': false,
+      'singleEvents': true,
+      'maxResults': 10,
+      'orderBy': 'startTime'
+    });
+
+    request.execute(function(resp) {
+      var events = resp.items;
+      appendPre('Upcoming events:');
+
+      if (events.length > 0) {
+        for (var i = 0; i < events.length; i++) {
+          var event = events[i];
+          var when = event.start.dateTime;
+          if (!when) {
+            when = event.start.date;
+          }
+          appendPre(event.summary + ' (' + when + ')')
+        }
+      } else {
+        appendPre('No upcoming events found.');
+      }
+
+    });
+  }
+
+  /**
+   * Append a pre element to the body containing the given message
+   * as its text node.
+   *
+   * @param {string} message Text to be placed in pre element.
+   */
+  function appendPre(message) {
+    var pre = document.getElementById('output');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+  }
+
+}
 }
 );
