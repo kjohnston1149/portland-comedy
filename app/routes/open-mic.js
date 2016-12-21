cfab225af5448ba4d4dd17f705513f6e4ba912ac
Kevin Johnston <kjohnston1149@gmail.com>
2016-12-20 16:06:17 -0800

    Got the google calendar API to dislpay text in the html

--- app/routes/open-mic.js
+++ app/routes/open-mic.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
+//   model: function() {
+//     debugger;
 //     var CLIENT_ID = '474937019011-ck824t8cts8ls81hovh552nn10oovlgd.apps.googleusercontent.com';
 //     var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 //     var calendarID = 'jim6kbma2850sds9mcohvbmgbo';
@@ -15,17 +16,16 @@ export default Ember.Route.extend({
 //       }
 //
 //      var url = "https://www.googleapis.com/calendar/v3/calendars/" + calendarID;
-//      $.when.function($, url){
+//      //checkAuth();
 //        return Ember.$.getJSON(url).then(function(responseJSON) {
-//        console.log(responsJSON);
-//        return responseJSON;
+//          console.log(responseJSON);
+//          return responseJSON;
+//        });
 //
-//    });
-//  }
 // }
 
-
-// model: function(gapi) {
+// 
+// model: function() {
 //   var CLIENT_ID = '474937019011-ck824t8cts8ls81hovh552nn10oovlgd.apps.googleusercontent.com';
 //
 //  var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
@@ -78,4 +78,4 @@ export default Ember.Route.extend({
 //
 // }
 }
-});
+);
